64a9b0dafa329fb77e44d606cd0fdff9fe9e2bb7
Enrique Ramos <enri46@gmail.com>
2016-03-27 19:38:12 +0200

    Inserción de chats

--- app/models/chat.php
+++ app/models/chat.php
@@ -66,10 +66,32 @@ function newChat($user_email, $receiver_email) {
 			$dbquery->bindParam(2, $receiver_email);
 			$dbquery->execute();
 			$number = $dbquery->rowCount();
-			$connection = null;
 			if ($number > 0) {
-				$result->setCode(TRUE);
-				$result->setStatus(OK);
+				// Recupero el Id autoincremental que se la ha asignado
+				$dbquery = $connection->prepare("SELECT Chat_id FROM Chat WHERE Chat_me = ? AND Chat_receiver = ?");
+				$dbquery->bindParam(1, $user_email);
+				$dbquery->bindParam(2, $receiver_email);
+				$dbquery->execute();
+				$chat_id = $dbquery->fetchColumn();
+
+				// Inserto el mismo chat de forma inversa para que el otro usuario pueda verlo
+				$dbquery = $connection->prepare("INSERT INTO Chat (Chat_id, Chat_me, Chat_receiver) values(?,?,?)");
+				$dbquery->bindParam(1, $chat_id);
+				$dbquery->bindParam(2, $receiver_email);
+				$dbquery->bindParam(3, $user_email);
+				$dbquery->execute();
+				$number = $dbquery->rowCount();
+				$connection = null;
+				if ($number > 0) {
+					$result->setCode(TRUE);
+					$result->setStatus(OK);
+					$result->setData($chat_id);
+				}	
+				else {	
+					$result->setCode(FALSE);
+					$result->setStatus(NOT_COMPLETED);
+					$result->setMessage("NOT INSERTED");
+				}
 			}	
 			else {	
 				$result->setCode(FALSE);
@@ -78,9 +100,17 @@ function newChat($user_email, $receiver_email) {
 			}
 		}
 		else{
+			// Recupero el Id
+			$dbquery = $connection->prepare("SELECT Chat_id FROM Chat WHERE Chat_me = ? AND Chat_receiver = ?");
+			$dbquery->bindParam(1, $user_email);
+			$dbquery->bindParam(2, $receiver_email);
+			$dbquery->execute();
+			$chat_id = $dbquery->fetchColumn();
+
 			$result->setCode(TRUE);
 			$result->setStatus(OK);
 			$result->setMessage("ALREADY EXISTS");
+			$result->setData($chat_id);
 		}
 	} catch (PDOException $e) {
 		$result->setCode(FALSE);
