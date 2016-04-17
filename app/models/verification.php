09fe5dbd3defcd9e1cd8810458355ade89b60cef
Enrique Ramos <enri46@gmail.com>
2016-04-17 20:32:51 +0200

    Restaurar contraseña olvidada

--- app/models/verification.php
+++ app/models/verification.php
@@ -1,5 +1,7 @@
 <?php 
 
+// New user verification
+
 $app->get("/api/verification/:email/:code", function($email, $code) use($app) {
 	$result = checkVerification($email, $code);
 	$show = "No se ha podido validar tu registro";
@@ -14,7 +16,9 @@ $app->get("/api/verification/:email/:code", function($email, $code) use($app) {
 		}
 	}
 
-	echo '<h2>'.$show.'</h2>';
+	$app->response->headers->set("Content-type", "text/html; charset=utf-8");
+	$app->response->status(OK);
+	$app->response->body('<h2>'.$show.'</h2>');
 });
 
 function checkVerification($email, $code){
@@ -62,7 +66,7 @@ function postVerificationUser($email, $password, $first, $alternative, $code) {
 		$dbquery->bindParam(2, $password);
 		$dbquery->bindParam(3, $first);
 		$dbquery->bindParam(4, $alternative);
-		$dbquery->bindParam(5, $code); // Genera código de verificación
+		$dbquery->bindParam(5, $code); // código de verificación
 		$dbquery->bindParam(6, date("Y-m-d H:i:s"));
 		$dbquery->execute();
 		$number = $dbquery->rowCount();
@@ -84,6 +88,154 @@ function deleteVerificationUser($email) {
 		$connection = getConnection();
 		$dbquery = $connection->prepare("DELETE FROM Verification WHERE User_email = ?");
 		$dbquery->bindParam(1, $email);
+		$dbquery->execute();
+		$number = $dbquery->rowCount();
+		$connection = null;
+		if ($number > 0) {
+			$result->setCode(TRUE);
+			$result->setStatus(OK);
+		}
+		else {
+			$result->setCode(FALSE);
+			$result->setStatus(NOT_COMPLETED);
+			$result->setMessage("NOT DELETED");
+		}
+	} catch (PDOException $e) {
+		$result->setCode(FALSE);
+		$result->setStatus(CONFLICT);
+		$result->setMessage("Error: " . $e->getMessage());
+	}
+	return $result;
+}
+
+
+// Restore password verification
+
+$app->get("/api/verification/restore-password/:email/:code", function($email, $code) use($app) {
+	$result = new Result();
+	$checked = checkCodePassword($email, $code);
+
+	$password = '';
+	$password1 = '';
+	$password2 = '';
+	if(isset($_GET['password1']))
+		$password1 = $_GET['password1'];
+	if(isset($_GET['password2']))
+		$password2 = $_GET['password2'];
+
+	if($password1 == $password2){
+		$result->setCode(TRUE);
+		$password = $password1;
+	}
+	else{
+		$result->setCode(FALSE);
+		$show = "Las contraseñas no coinciden. Vuelve al formulario desde el botón del mail.";
+	}
+
+	if(strlen($password) < 8){
+		$result->setCode(FALSE);
+		$show = "La longitud mínima es de 8 caracteres.";
+	}
+
+	if($result->getCode() == TRUE && $checked->getCode() == TRUE){
+		updateVerifiedPassword($email, $password);
+		$show = "Cambio de contraseña completado";
+		deleteVerificationPassword($email, $code);
+	}
+
+	if($checked->getCode() == FALSE){
+		$show = "No se ha podido comprobar tu petición de cambio de contraseña";
+	}
+
+	$app->response->headers->set("Content-type", "text/html; charset=utf-8");
+	$app->response->status(OK);
+	$app->response->body('<h2>'.$show.'</h2>');
+});
+
+function checkCodePassword($email, $code){
+	$result = new Result();
+	try {
+		$connection = getConnection();
+		$dbquery = $connection->prepare("SELECT * FROM Verification WHERE User_email = ? AND Verification_code = ?");
+		$dbquery->bindParam(1, $email);
+		$dbquery->bindParam(2, $code);
+		$dbquery->execute();
+		$data = $dbquery->fetchObject();
+		$connection = null;
+
+		if ($data != null) {
+			$result->setCode(TRUE);
+			$result->setStatus(OK);
+			$result->setData($data);
+		}	
+		else {
+			$result->setCode(FALSE);
+			$result->setStatus(NOT_COMPLETED);
+			$result->setMessage("Does the Verification exist?");
+		}
+	} catch (PDOException $e) {
+		$result->setCode(FALSE);
+		$result->setStatus(CONFLICT);
+		$result->setMessage("Error: " . $e->getMessage());
+	}
+	return $result;
+}
+
+function insertVerificationPassword($email, $code) {
+	try {
+		$connection = getConnection();
+		$dbquery = $connection->prepare("INSERT INTO Verification (User_email, Verification_code, Verification_creationTime) VALUES(?, ?, ?)");
+		$dbquery->bindParam(1, $email);
+		$dbquery->bindParam(2, $code); // código de verificación
+		$dbquery->bindParam(3, date("Y-m-d H:i:s"));
+		$dbquery->execute();
+		$number = $dbquery->rowCount();
+		$connection = null;
+		if ($number > 0) {
+			return TRUE;
+		}	
+		else {	
+			return FALSE;
+		}
+	} catch (PDOException $e) {
+		return FALSE;
+	}
+}
+
+function updateVerifiedPassword($email, $password){
+	$result = new Result();
+	try {
+		$connection = getConnection();
+		$dbquery = $connection->prepare("UPDATE User SET User_password = sha1(?) WHERE User_email = ?");
+		$dbquery->bindParam(1, $password);
+		$dbquery->bindParam(2, $email);
+		$dbquery->execute();
+		$number = $dbquery->rowCount();
+		$connection = null;
+		if ($number > 0) {
+			$result->setCode(TRUE);
+			$result->setStatus(OK);
+		}
+		else {
+			$result->setCode(FALSE);
+			$result->setStatus(NOT_COMPLETED);
+			$result->setMessage("NOT UPDATED");
+		}
+	} catch (PDOException $e) {
+		$result->setCode(FALSE);
+		$result->setStatus(CONFLICT);
+		$result->setMessage("Error: " . $e->getMessage());
+	}
+	return $result;	
+}
+
+function deleteVerificationPassword($email, $code) {
+	$result = new Result();
+	try {
+		$connection = getConnection();
+		$dbquery = $connection->prepare("DELETE FROM Verification WHERE User_email = ? AND Verification_code = ?");
+		$dbquery->bindParam(1, $email);
+		$dbquery->bindParam(2, $code);
 		$dbquery->execute();
 		$number = $dbquery->rowCount();
 		$connection = null;
--- app/routes.php
+++ app/routes.php
@@ -34,6 +34,31 @@ $app->get("/", function() use($app) {
 	<a href ='doc/index.html'>API Documentation</a>");
 });
 
+$app->get("/restore-password/:email/:code", function($userEmail, $code) use($app) {
+	include_once("models/verification.php");
+	$checked = checkCodePassword($userEmail, $code);
+	if($checked->getCode() == TRUE){
+		$app->response->headers->set("Content-type", "text/html; charset=utf-8");
+		$app->response->status(OK);
+		$app->response->body(
+		'<h1>Formulario de cambio de contraseña!!</h1>
+
+		 <form action="http://www.enriqueramos.info/osporthello/api/verification/restore-password/'.$userEmail.'/'.$code.'" method="get">
+  			New password:<br>
+  			<input type="password" name="password1" value=""><br>
+  			Repeat new password:<br>
+  			<input type="password" name="password2" value=""><br><br>
+  			<input type="submit" value="Change password">
+		</form> 
+		');
+	}
+	else{
+		$app->response->headers->set("Content-type", "text/html; charset=utf-8");
+		$app->response->status(OK);
+		$app->response->body('<h2>No se ha podido comprobar tu petición de cambio de contraseña</h2>');
+	}
+});
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // USER 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -128,6 +153,105 @@ include_once("models/verification.php");
 // EMAIL /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
+
+$app->get("/api/restore-password/:email", function($userEmail) use($app) {
+	$code = generarApiKey($userEmail+"verification_code");
+
+	$inserted = insertVerificationPassword($userEmail, $code); // Añadir un User a Verification
+
+	if($inserted == TRUE){
+		$result = sendPasswordEmail($userEmail, $code); // Enviar mail de cambio de password
+	}
+	else {
+		$result = new Result();
+		$result->setCode(FALSE);
+		$result->setMessage("Error sending restore password email");
+	}
+	
+	$app->response->status($result->getCode());
+	$app->response->body(json_encode($result));
+});
+
+function sendPasswordEmail($emailAddress, $code){
+	header('Content-type: application/json;charset=utf8');
+	require_once('phpmailer524/class.phpmailer.php');
+	require_once "config.php";
+	$response = new Result();
+
+	$mail = new PHPMailer(true); // true param means it will throw exceptions on errors
+	$mail->IsSMTP(); // telling the class to use SMTP
+	try {
+		$mail->SMTPAuth = true;
+		$mail->SMTPSecure = "tls";
+		$mail->Host = "smtp.gmail.com"; // sets GMAIL as the SMTP server
+		$mail->Port = 587; // set the SMTP port for the GMAIL server
+		$mail->Username = "osporthello@gmail.com"; // GMAIL username
+		$mail->Password = "ies29700412"; // GMAIL password
+		$mail->AddAddress($emailAddress); // Receiver email
+		$mail->SetFrom("osporthello@gmail.com"); // email sender
+		$mail->Subject = "Restore your password in OSport Hello"; // subject of the message
+		//content in HTML
+		$mail->MsgHTML(htmlRestorePasswordButton($emailAddress, $code));
+		//Alternative plain text content (if email client blocks html content)
+		$mail->AltBody = 'Copy this link in your browser to restore your password: https://enriqueramos.info/osporthello/api/restore-password/'.$emailAddress.'/'.$code;
+		// Send email!
+		$mail->Send();
+		$response->setCode(TRUE);
+		$response->setMessage("Message Sent OK to " . $emailAddress);
+	}	 catch (phpmailerException $e) {
+		$response->setCode(FALSE);
+		$response->setMessage("Error: " . $e->errorMessage());
+	} catch (Exception $e) {
+		$response->setCode(FALSE);
+		$response->setMessage("Error: " . $e->getMessage());
+	}
+
+	return $response;
+}
+
+function htmlRestorePasswordButton($email, $code){
+	$htmlC = 
+	'Click this button to go to restore password form:
+	<table width="100%">
+    <tr>
+      <td>
+        <table border="0" cellpadding="0" cellspacing="0"> 
+          <tr>
+            <td height="20" width="100%" style="font-size: 20px; line-height: 20px;">
+            &nbsp;
+            </td>
+          </tr>
+        </table>
+        <table border="0" align="left" cellpadding="0" cellspacing="0">
+          <tbody>
+          <tr>
+            <td align="left">
+              <table border="0" cellpadding="0" cellspacing="0" width="150">
+                <tr>
+                  <td align="center" bgcolor="#43A047" width="150" style="-moz-border-radius: 4px; -webkit-border-radius: 4px; border-radius: 4px;">
+                    <a href="https://enriqueramos.info/osporthello/restore-password/'.$email.'/'.$code.'" 
+                    style="padding: 10px;width:150px;display: block;text-decoration: none;border:0;text-align: center;font-weight: bold;font-size: 15px;font-family: sans-serif;color: #ffffff;background: #43A047;border: 1px solid #43A047;-moz-border-radius: 4px; -webkit-border-radius: 4px; border-radius: 4px;line-height:17px;" class="button_link">
+                    Go to restore password form
+                    </a>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0"> 
+          <tr>
+            <td height="20" width="100%" style="font-size: 20px; line-height: 20px;">
+            &nbsp;
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>';
+  return $htmlC;
+}
 
 $app->post("/api/registration", function() use($app) {
 	//get params
